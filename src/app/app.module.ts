72cd3ff087c8f59afe3a1db42950ef9d7f55a1e8
Kaleid002 <aenevia666@gmail.com>
2025-06-20 17:40:20 +0800

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
